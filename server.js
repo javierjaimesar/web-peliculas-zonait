b278ef7aa2fefe5586fdf0c9cc100a997169066e
Javier Jaimes <javierjaimes2003@gmail.com>
2023-10-23 18:32:21 -0300

    Prueba 2 - Rutas con Express

--- server.js
+++ server.js
@@ -1,37 +1,20 @@
 const express = require('express');
 const path = require('path');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-const { StaticRouter } = require('react-router-dom');
-const { default: SearchMovies } = require('./src/assets/pages/SearchMovies');
 
 const app = express();
 
+// Middleware para servir archivos estáticos del cliente
+app.use(express.static(path.join(__dirname, 'build')));
+
+// // Configurar rutas para la API (si es necesario)
+// app.use('/api', require('./apiRoutes'));
+
 // Middleware para manejar rutas en el servidor
-app.get('/search', (req, res) => {
-  const context = {};
-  const appHtml = ReactDOMServer.renderToString(
-    <StaticRouter location={req.url} context={context}>
-      <SearchMovies />
-    </StaticRouter>
-  );
-
-  const html = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>React Router SSR</title>
-      </head>
-      <body>
-        <div id="root">${appHtml}</div>
-      </body>
-    </html>
-  `;
-
-  res.status(200).send(html);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
