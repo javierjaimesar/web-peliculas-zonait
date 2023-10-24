4fd5cb83ed002dbcce977ae750d733d05162841e
Javier Jaimes <javierjaimes2003@gmail.com>
2023-10-24 00:21:11 -0300

    Prueba 7

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware para servir archivos estáticos del cliente (Vite)
-app.use(express.static(path.join(__dirname)));
+// app.use(express.static(path.join(__dirname)));
 
+app.use(express.static(__dirname,'dist'));
+
+// app.get('/', (req, res) => {
+//     return res.send('Hello World!');
+// });
+
+// app.get('/public/hola', (req, res) => {
+//     return res.send('Hola!');
+// })
 // Middleware para manejar rutas en el servidor
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    console.log(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
 
 app.listen(PORT, () => {
     console.log(`Servidor Express escuchando en el puerto ${PORT}`);
